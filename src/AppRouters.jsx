08950c764a8b758d1cde79b144301733f7eb0a90
kostal79 <kostal1979@gmail.com>
2023-03-19 14:04:26 +0300

    made new db for types and for images, constructor

--- src/AppRouters.jsx
+++ src/AppRouters.jsx
@@ -4,28 +4,27 @@ import Contacts from "./pages/Contacts";
 import Delivery from "./pages/Delivery";
 import ErrorPage from "./pages/ErrorPage";
 import Feedback from "./pages/Feedback";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import OrderPage from "./pages/OrderPage";
 import Rent from "./pages/Rent";
 import Rules from "./pages/Rules";
 import WhereToRide from "./pages/WhereToRide";
 
-
 const AppRouters = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/feedback" element={<Feedback />} />
-            <Route path="/rent" element={<Rent />} />
-            <Route path="/wheretoride" element={<WhereToRide />} />
-            <Route path="/rules" element={<Rules />} />
-            <Route path="/order" element={<OrderPage />} />
-            <Route path="/*" element={<Home />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/bikepark-2" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contacts" element={<Contacts />} />
+      <Route path="/delivery" element={<Delivery />} />
+      <Route path="/feedback" element={<Feedback />} />
+      <Route path="/rent" element={<Rent />} />
+      <Route path="/wheretoride" element={<WhereToRide />} />
+      <Route path="/rules" element={<Rules />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="/*" element={<ErrorPage />} />
+    </Routes>
+  );
+};
 
-export default AppRouters
+export default AppRouters;
