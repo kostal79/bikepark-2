8a0884117c41bfa9b92387699a357fb0bdba1747
Alex <74615724+kostal79@users.noreply.github.com>
2023-05-24 12:23:47 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -10,19 +10,6 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyDsnw4L54KgH16_MUWWOuJfX50EGyhD7R0",
-
-  authDomain: "bikepark-2.firebaseapp.com",
-
-  projectId: "bikepark-2",
-
-  storageBucket: "bikepark-2.appspot.com",
-
-  messagingSenderId: "207967450354",
-
-  appId: "1:207967450354:web:2d35e9241765720db28c6b",
-
-  measurementId: "G-NTXDFYFQ8L"
 
 };
 
